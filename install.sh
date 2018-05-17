71484faf1263b255cdcc5ac9e409730714e2a440
Nathan JANCZEWSKI <oxodao@protonmail.com>
2018-05-17 21:22:27 +0000

    ZSH install is working

--- install.sh
+++ install.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
+echo "Your current folder is $(pwd). Please be sure the folder that won't move."
+
+echo "Removing old symlink & creating directories"
 rm ~/.files
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3blocks
 mkdir -p ~/.config/dunst
 
+echo "Creating symlink for main folder"
 ln -s $(pwd)/files        ~/.files
+
+echo "Installing zsh"
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s $(pwd)/zsh/theme ~/.oh-my-zsh/themes/mattboll.zsh-theme
+ln -s $(pwd)/zsh/zshrc ~/.zshrc
+
+echo "Installing i3-gaps"
 ln -s $(pwd)/i3/config    ~/.config/i3/config
 ln -s $(pwd)/i3/i3blocks  ~/.config/i3blocks/config
 ln -s $(pwd)/i3/dunstrc   ~/.config/dunst/dunstrc
 ln -s $(pwd)/i3/resources ~/.Xresources
+
+echo "Installing Git config"
 ln -s $(pwd)/git/config   ~/.gitconfig
 ln -s $(pwd)/git/ignore   ~/.gitignore
