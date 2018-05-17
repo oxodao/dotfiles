f612d70d87a72f44cb482abab737c9c988218bea
Nathan JANCZEWSKI <oxodao@protonmail.com>
2018-05-17 21:28:11 +0000

    Termite theme

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@ echo "Your current folder is $(pwd). Please be sure the folder that won't move."
 
 echo "Removing old symlink & creating directories"
 rm ~/.files
+rm ~/.config/termite/config
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3blocks
 mkdir -p ~/.config/dunst
+mkdir -p ~/.config/termite
 
 echo "Creating symlink for main folder"
 ln -s $(pwd)/files        ~/.files
@@ -20,6 +22,9 @@ ln -s $(pwd)/i3/config    ~/.config/i3/config
 ln -s $(pwd)/i3/i3blocks  ~/.config/i3blocks/config
 ln -s $(pwd)/i3/dunstrc   ~/.config/dunst/dunstrc
 ln -s $(pwd)/i3/resources ~/.Xresources
+
+echo "Installing termite theme"
+ln -s $(pwd)/termite/config ~/.config/termite/config
 
 echo "Installing Git config"
 ln -s $(pwd)/git/config   ~/.gitconfig
