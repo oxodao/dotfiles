976a6caf7341587f50ec38633d82ecd9fbe20f53
Nathan JANCZEWSKI <oxodao@protonmail.com>
2018-05-19 14:12:40 +0200

    Adding tmux config

--- install.sh
+++ install.sh
@@ -4,9 +4,10 @@ mkdir -p ~/.config/i3
 mkdir -p ~/.config/i3blocks
 mkdir -p ~/.config/dunst
 
-ln -s $(pwd)/files       ~/.files
-ln -s $(pwd)/i3/config   ~/.config/i3/config
-ln -s $(pwd)/i3/i3blocks ~/.config/i3blocks/config
-ln -s $(pwd)/i3/dunstrc  ~/.config/dunst/dunstrc
-ln -s $(pwd)/git/config  ~/.gitconfig
-ln -s $(pwd)/git/ignore  ~/.gitignore
+ln -s $(pwd)/files          ~/.files
+ln -s $(pwd)/i3/config      ~/.config/i3/config
+ln -s $(pwd)/i3/i3blocks    ~/.config/i3blocks/config
+ln -s $(pwd)/i3/dunstrc     ~/.config/dunst/dunstrc
+ln -s $(pwd)/git/config     ~/.gitconfig
+ln -s $(pwd)/git/ignore     ~/.gitignore
+ln -s $(pwd)/tmux/tmux.conf ~/.tmux.conf
