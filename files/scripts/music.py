ba9c386d4f575bcbe70dfb2c03db59cdff583dba
Nathan JANCZEWSKI <nathan@janczewski.fr>
2020-09-11 17:26:53 +0200

    Blabla

--- files/scripts/music.py
+++ files/scripts/music.py
@@ -6,10 +6,13 @@ FNULL = open(os.devnull, 'w');
 retcode = subprocess.call(['playerctl', 'status'], stdout=FNULL, stderr=subprocess.STDOUT);
 
 if(retcode == 0):
-    a = subprocess.check_output(['playerctl', 'metadata', 'xesam:albumArtist']);
-    b = subprocess.check_output(['playerctl', 'metadata', 'xesam:title']);
-    c = a.decode("utf-8") + " - " + b.decode("utf-8");
-    print("♫ " + c[:35]);
+    a = subprocess.check_output(['playerctl', 'metadata', 'xesam:albumArtist']).strip();
+    b = subprocess.check_output(['playerctl', 'metadata', 'xesam:title']).strip();
+    c = a.decode("utf-8") + " - " + b.decode("utf-8")[:35];
+    if (c.strip() == "-"):
+        print("♫ None ");
+    else:
+        print("♫ " + c[:35]);
 else:
     print("♫  None  ");
 
