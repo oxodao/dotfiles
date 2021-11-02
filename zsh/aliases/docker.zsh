2d8f614ebf1773b61a339f59dc54a67289d63aaa
Nathan JANCZEWSKI <nathan@janczewski.fr>
2021-11-02 16:40:09 +0100

    Fixing dr/sr

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -5,9 +5,10 @@ alias 'dc'='docker-compose'
 
 # Those envvar are loaded on a per-project basis with the help of direnv
 alias 'd'='docker-compose exec $PHP_CONTAINER'
-alias 'dr'='docker-compose run $PHP_CONTAINER'
-alias 'n'='docker-compose exec $NODE_CONTAINER'
 alias 's'='docker-compose exec $PHP_CONTAINER bin/console '
-alias 'sr'='docker-compose run $PHP_CONTAINER bin/console '
+alias 'n'='docker-compose exec $NODE_CONTAINER'
+
+alias 'dr'='docker-compose run --rm $PHP_CONTAINER'
+alias 'sr'='docker-compose run --rm $PHP_CONTAINER bin/console '
 
 
