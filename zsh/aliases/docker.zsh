ff477db7c49c90e9d14c05663542732b74bac857
Nathan JANCZEWSKI <nathan@janczewski.fr>
2022-01-17 18:16:32 +0100

    Alacritty theme (ChallengerDeep) + deprecating old docker-compose

--- zsh/aliases/docker.zsh
+++ zsh/aliases/docker.zsh
@@ -1,15 +1,15 @@
 # docker kill all
 alias 'dka'='docker kill $(docker ps -q)'
 
-alias 'dc'='docker-compose'
+alias 'dc'='docker compose'
 
 # Those envvar are loaded on a per-project basis with the help of direnv
-alias 'd'='docker-compose exec $PHP_CONTAINER'
-alias 's'='docker-compose exec $PHP_CONTAINER bin/console '
-alias 'n'='docker-compose exec $NODE_CONTAINER'
+alias 'd'='docker compose exec $PHP_CONTAINER'
+alias 's'='docker compose exec $PHP_CONTAINER bin/console '
+alias 'n'='docker compose exec $NODE_CONTAINER'
 
-alias 'dr'='docker-compose run --rm $PHP_CONTAINER'
-alias 'sr'='docker-compose run --rm $PHP_CONTAINER bin/console '
+alias 'dr'='docker compose run --rm $PHP_CONTAINER'
+alias 'sr'='docker compose run --rm $PHP_CONTAINER bin/console '
 
 
 # that thing there 
