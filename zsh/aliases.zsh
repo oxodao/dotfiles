6b80264d7d764cfe20ab77f1089cb7010b642a03
Nathan JANCZEWSKI <nathan@janczewski.fr>
2022-11-14 17:39:08 +0100

    Edit desktop

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,4 +3,8 @@ source $HOME/.config/zsh/aliases/folders.zsh
 source $HOME/.config/zsh/aliases/git.zsh
 source $HOME/.config/zsh/aliases/global.zsh
 
+# Software specific aliases
+alias ytmp3="yt-dlp -x --audio-format mp3 --audio-quality 0"
+alias x=aunpack
+
 alias ffmpeg_crop="ffmpeg -i INPUT -filter:v \"crop=out_w:out_h:x:y\" out.mp4"
