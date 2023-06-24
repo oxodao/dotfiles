28b8f29cfd1a9c0ebe57f3ff05d7d769b58c0e56
Nathan JANCZEWSKI <nathan@janczewski.fr>
2023-06-24 12:58:19 +0200

    Desktop updated

--- config/zsh/aliases/docker.zsh
+++ config/zsh/aliases/docker.zsh
@@ -7,6 +7,7 @@ alias 'dc'='docker compose'
 # Those envvar are loaded on a per-project basis with the help of direnv
 alias 'd'='docker compose exec $PHP_CONTAINER'
 alias 's'='docker compose exec $PHP_CONTAINER bin/console '
+alias 'p'='docker compose exec $PHP_CONTAINER python manage.py'
 alias 'n'='docker compose exec $NODE_CONTAINER'
 
 alias 'dr'='docker compose run --rm $PHP_CONTAINER'
