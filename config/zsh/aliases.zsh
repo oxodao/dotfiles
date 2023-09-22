0fbc7da3c9d967381d73c320608cd4ca154a80f5
Nathan JANCZEWSKI <nathan@janczewski.fr>
2023-09-22 22:33:23 +0200

    Updated dotfiles

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -9,3 +9,6 @@ alias x=aunpack
 
 alias ffmpeg_crop="ffmpeg -i INPUT -filter:v \"crop=out_w:out_h:x:y\" out.mp4"
 alias ffmpeg_snap="ffmpeg -i INPUT -c:v libx265 -c:a aac -vf format=yuv420p -x265-params crf=25 out.mp4"
+
+alias 'syncmc'='rsync -azv -P /opt/overflow/skcraft_ws/_upload/ o:~/mc/launcher/downloads/'
+alias 'synccalibre'='rsync -azv -P ~/Calibre\ Library/ o:~/calibreweb/books/'
--- zshrc
+++ zshrc
@@ -1,4 +1,6 @@
 PATH=$PATH:$HOME/.config/zsh/scripts
 source $HOME/.config/zsh/global.zsh
 
-alias 'syncmc'='rsync -azv -P /opt/mc_workspace/workspace/_upload/ o:/home/oxodao/mc/launcher/downloads/'
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
