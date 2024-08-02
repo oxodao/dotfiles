0ff772175c70ee4be6bd5ebf01c9d19e03636bb6
Nathan JANCZEWSKI <nathan@janczewski.fr>
2024-08-02 21:11:50 +0200

    wip

--- config/zsh/software/rsync.zsh
+++ config/zsh/software/rsync.zsh
@@ -1,3 +1,8 @@
 alias 'syncmc'='rsync -azv -P /opt/overflow/skcraft_ws/_upload/ o:~/mc/launcher/downloads/'
 alias 'syncmcg'='rsync -azv -P /opt/overflow/skcraft_ws/_upload/ o:~/mc/gamut/data/files/launcher/downloads/'
+
+alias 'syncmcpp'='rsync -azv -P /opt/overflow/skcraft_pp_ws/_upload/ o:~/mc/launcher-pp/downloads/'
+
+alias 'syncmcserv'='rsync -azv --exclude world --exclude backups --exclude logs --exclude journeymap --exclude crash-reports -P /opt/overflow/current_server/ o:~/mc/server/data'
+
 alias 'synccalibre'='rsync -azv -P ~/Calibre\ Library/ o:~/calibreweb/books/'
