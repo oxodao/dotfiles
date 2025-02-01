74ed53021b2bf96b9cffaf13093d8d14dfc4639a
Nathan JANCZEWSKI <nathan@janczewski.fr>
2025-02-01 15:52:19 +0100

    fix java

--- config/zsh/software/java.zsh
+++ config/zsh/software/java.zsh
@@ -1,6 +1,9 @@
-function _zsh_setup_java() {
-  export SDKMAN_DIR="$HOME/.sdkman"
-  [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
-}
+if [[ ${ZSH_IGNORE_JAVA:-0} != 1 ]]; then
+  if [[ -f "$HOME/.sdkman/bin/sdkman-init.sh" ]]; then
+    export SDKMAN_DIR="$HOME/.sdkman"
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+  else
+    echo "SDKMAN (Java) is not installed. Install it or disable integration with 'export ZSH_IGNORE_JAVA=1' in ~/.zshrc.custom"
+  fi
+fi
 
-_zsh_check_software JAVA sdk "SDKMAN (Java)" _zsh_setup_java
